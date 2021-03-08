87334a2bd4a687ebdedc24fbee218420f36c733f
Anoshkin <a.anoshkin@profsoft.pro>
2021-03-08 17:40:43 +0300

    project started

---
+++ TestApp/TestApp/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  TestApp
+//
+//  Created by Andrey on 08/03/2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ TestApp/TestApp/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  TestApp
+//
+//  Created by Andrey on 08/03/2021.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
